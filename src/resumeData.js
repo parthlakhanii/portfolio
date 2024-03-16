9813bcca929876400d60677a7c910af980ade9ab
parthlakhanii <lakha117@uwindsor.ca>
2024-03-16 16:04:59 -0400

    create portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,117 @@
+let resumeData = {
+  imagebaseurl: 'https://portfolio.github.io/',
+  name: 'Parth Lakhani',
+  role: 'Software Developer',
+  linkedinId: 'https://www.linkedin.com/in/parthlakhanii/',
+  skypeid: 'Your skypeid',
+  roleDescription:
+    ' Experienced Full Stack Developer with a demonstrated history of working in the IT industry. While working as a Software developer I have worked in various technologies following agile methodologies with weekly sprints. Gained knowledge working in Frontend and Backend. Exploring new technologies and concepts every day, always open to new opportunities.',
+  socialLinks: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/parthlakhanii/',
+      className: 'fa fa-linkedin',
+    },
+    {
+      name: 'github',
+      url: 'https://github.com/parthlakhanii',
+      className: 'fa fa-github',
+    },
+    // {
+    //   name: 'twitter',
+    //   url: '',
+    //   className: 'fa fa-twitter',
+    // },
+  ],
+  aboutme:
+    'A seasoned software developer with over 5 years of experience at SAP, proficient in Angular, React, JavaScript, and Java. Specializing in crafting innovative solutions that blend technical expertise with creative flair, driven by a passion for continuous learning and user-centric design. Committed to pushing the boundaries of innovation and making a lasting impact through work.',
+  address: '7 Crescent place, East York - ON',
+  education: [
+    {
+      UniversityName: 'University of Windsor',
+      specialization: 'Master of Applied Computing',
+      MonthOfPassing: 'May',
+      YearOfPassing: '2021',
+      Achievements: 'Some Achievements',
+    },
+    {
+      UniversityName: 'A. D. Patel Institute of Technology',
+      specialization: 'Information Technology',
+      MonthOfPassing: 'May',
+      YearOfPassing: '2018',
+      Achievements: 'Some Achievements',
+    },
+  ],
+  work: [
+    {
+      CompanyName: 'SAP',
+      specialization: 'Software Developer',
+      MonthOfLeaving: 'October 2020 -',
+      YearOfLeaving: 'Present',
+      Achievements: 'Some Achievements',
+    },
+    {
+      CompanyName: 'SRKay Consulting Group',
+      specialization: 'Associate Software Developer',
+      MonthOfLeaving: 'January 2018 -',
+      YearOfLeaving: 'July 2019',
+      Achievements: 'Some Achievements',
+    },
+  ],
+  skillsDescription: 'Your skills here',
+  skills: [
+    {
+      skillname: 'JavaScript',
+    },
+    {
+      skillname: 'Java',
+    },
+    {
+      skillname: 'React',
+    },
+    {
+      skillname: 'Angular',
+    },
+    {
+      skillname: 'TypeScript',
+    },
+    {
+      skillname: 'Python',
+    },
+  ],
+  portfolio: [
+    {
+      name: 'News Aggregator',
+      description:
+        'Minimal news aggregator app build using React, Node, Mongodb and, Material-ui',
+      imgurl: 'images/news-aggregator.png',
+      url: 'https://github.com/parthlakhanii/news-aggregator',
+    },
+    {
+      name: 'Wen Search Engine',
+      description: 'Basic web search engine using Java and Angular',
+      imgurl: 'images/WSE.png',
+      url: 'https://github.com/parthlakhanii/webSearch-backend',
+    },
+    {
+      name: 'Diet-Mate',
+      description: 'Python and Angular based Diet Management System',
+      imgurl: 'images/DM.png',
+      url: 'https://gitlab.cs.uwindsor.ca/lakha117/diet-mate-webapp',
+    },
+  ],
+  testimonials: [
+    {
+      description:
+        'I worked with Parth in my first company. He is great at executing the things. He is a team player with good analytical skills. His approach is always agile oriented. He lead few of the project modules and delivered it on time. He is very focussed towards his work. Highly recommended!',
+      name: 'Aman Kanaujia',
+    },
+    {
+      description:
+        'Parth is very sincere and hard working individual. He is good with his Front End skills, and, contributed to important features of SCIKEY application. I wish him all the best for his future endeavours.',
+      name: 'Sujit Udhane',
+    },
+  ],
+};
+
+export default resumeData;
