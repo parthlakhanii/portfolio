c3cbbe0e8a77529753f17c899d4d4f73905fbf6f
parthlakhanii <parthmlakhani55@gmail.com>
2025-02-03 18:11:39 -0500

    update url

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class About extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
-      <section id='about'>
-        <div className='row'>
-          <div className='three columns'>
-            <img className='profile-pic' src='images/Parth.jpg' alt='' />
+      <section id="about">
+        <div className="row">
+          <div className="three columns">
+            <img
+              src="portfolio/images/Parth.jpg"
+              alt="Parth"
+              className="profile-pic"
+            />
           </div>
 
-          <div className='nine columns main-col'>
+          <div className="nine columns main-col">
             <h2>About Me</h2>
             <p>{resumeData.aboutme}</p>
 
-            <div className='row'>
-              <div className='columns contact-details'>
+            <div className="row">
+              <div className="columns contact-details">
                 <h2>Contact Details</h2>
-                <p className='address'>
+                <p className="address">
                   <span>{resumeData.name}</span>
                   <br></br>
                   <span>{resumeData.address}</span>
