e24c22541da5a78eac4dfe42c901457aba5694d4
parthlakhanii <parthmlakhani55@gmail.com>
2025-02-03 19:04:30 -0500

    update imgUrl

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class Porfolio extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
-      <section id='portfolio'>
-        <div className='row'>
-          <div className='twelve columns collapsed'>
+      <section id="portfolio">
+        <div className="row">
+          <div className="twelve columns collapsed">
             <h1>Check Out Some of My Works</h1>
             <div
-              id='portfolio-wrapper'
-              className='bgrid-quarters s-bgrid-thirds cf'
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
             >
               {resumeData.portfolio &&
                 resumeData.portfolio.map((item) => {
                   return (
-                    <div className='columns portfolio-item'>
-                      <div className='item-wrap'>
-                        <a target='_window' href={item.url}>
-                          <img src={`${item.imgurl}`} className='item-img' />
-                          <div target='_window' className='overlay'>
-                            <div className='portfolio-item-meta'>
+                    <div className="columns portfolio-item">
+                      <div className="item-wrap">
+                        <a target="_window" href={item.url}>
+                          <img src={`${item.imgUrl}`} className="item-img" />
+                          <div target="_window" className="overlay">
+                            <div className="portfolio-item-meta">
                               <h5>{item.name}</h5>
                               <p>{item.description}</p>
                             </div>
